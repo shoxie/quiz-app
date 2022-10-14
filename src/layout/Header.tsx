61194112a2986c3a9b386e7f408f4cb1958e0285
shoxie <sktt1lka@gmail.com>
2022-10-14 21:54:01 +0700

    horse

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,6 +7,7 @@ import {
   HStack,
   Text,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
@@ -17,28 +18,45 @@ export default function Header() {
   const { status, data } = useSession();
 
   return (
-    <Container maxW="container.xl" py="5">
-      <HStack justify="space-between" width="full">
-        <Box>
-          <Heading>English Quiz app</Heading>
-        </Box>
-        <Box>
-          <HStack>
-            {status === "authenticated" ? (
-              <Button onClick={() => signOut()}>
-                <Text>
-                  Welcome, <u>{data.user?.name}</u>
-                </Text>
+    <Box w="full" position="fixed" py="5">
+      <Container
+        maxW="container.xl"
+        w="full"
+        top={0}
+        left={0}
+        as={motion.div}
+        initial={{
+          y: -100,
+        }}
+        animate={{
+          y: 0,
+          transition: {
+            duration: 0.5,
+          },
+        }}
+      >
+        <HStack justify="space-between" width="full">
+          <Box>
+            <Heading>English Quiz app</Heading>
+          </Box>
+          <Box>
+            <HStack>
+              {status === "authenticated" ? (
+                <Button onClick={() => signOut()}>
+                  <Text>
+                    Welcome, <u>{data.user?.name}</u>
+                  </Text>
+                </Button>
+              ) : (
+                <Link href="/sign-in">Sign in</Link>
+              )}
+              <Button variant="unstyled" onClick={toggleColorMode}>
+                {colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
               </Button>
-            ) : (
-              <Link href="/sign-in">Sign in</Link>
-            )}
-            <Button variant="unstyled" onClick={toggleColorMode}>
-              {colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
-            </Button>
-          </HStack>
-        </Box>
-      </HStack>
-    </Container>
+            </HStack>
+          </Box>
+        </HStack>
+      </Container>
+    </Box>
   );
 }
